0827474bee1b895ad09ef77931c7f3462d216672
Johash Perera <johash.p@azendtech.com>
2025-05-07 23:06:03 +0530

    chore: add unauthorized exception filter for better response

---
+++ src/common/filters/unauthorized-exception.filter.ts
@@ -0,0 +1,23 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class UnauthorizedExceptionFilter implements ExceptionFilter {
+  catch(exception: UnauthorizedException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const status = exception.getStatus();
+
+    response.status(status).json({
+      statusCode: status,
+      message: 'User not authorized. Please login.',
+      redirectUri: '/login',
+    });
+  }
+}
